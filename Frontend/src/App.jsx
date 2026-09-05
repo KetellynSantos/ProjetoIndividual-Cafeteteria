9bfdc61a7167479249da89da7dfbe460eeb654bf
Ketellyn Santos <ketellyn.santos@sptech.school>
2026-09-05 14:19:07 -0300

    refactor(front): atualizar App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from 'react'
 import Header from './Components/Header/Header'
 import Home from './pages/Home/Home'
 import CadastroProduto from './pages/CadastroProdutos/CadastroProdutos';
-import Cardapio from "./pages/Cardapio/Cardapio";
+import {Cardapio} from "./pages/Cardapio/Cardapio";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
